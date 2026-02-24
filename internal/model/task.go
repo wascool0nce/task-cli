d5b3a44fba54bf7cb46752049e2e0dc5d79d1967
Maxim <m.demchenko2001@gmail.com>
2026-02-24 23:10:41 +0300

    add new model

---
+++ internal/model/task.go
@@ -0,0 +1,11 @@
+package model
+
+import "time"
+
+type Task struct {
+	ID          int       `json:"id"`
+	Description string    `json:"description"`
+	Status      string    `json:"status"`
+	CreatedAt   time.Time `json:"createdAt"`
+	UpdatedAt   time.Time `json:"updatedAt"`
+}
