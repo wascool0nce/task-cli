d38b83de6e66e5409230c94f72512d6266b331a3
Maxim <m.demchenko2001@gmail.com>
2026-02-25 17:15:45 +0300

    add validate

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -1,4 +1,21 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
 func main() {
+	_, err := validateArgs(os.Args[1:])
+	if err != nil {
+		log.Fatalf("Ошибка запуска taks-cli: %v", err)
+	}
+}
+
+func validateArgs(args []string) ([]string, error) {
+	if len(args) < 1 {
+		return nil, fmt.Errorf("не хватает аргументов командной строки")
+	}
+	return args, nil
 }
