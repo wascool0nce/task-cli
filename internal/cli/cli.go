4be8c23af245352dd2696e9dab8407937cc2bf63
Maxim <cyberhunt@MacBook-Air-Maxim.local>
2026-05-26 09:58:44 +0300

    TEST-2 add full realese

--- internal/cli/cli.go
+++ internal/cli/cli.go
@@ -4,15 +4,24 @@ import (
 	"errors"
 	"fmt"
 	"io"
-	"task-cli/internal/usecase"
+	"strconv"
+	"task-cli/internal/domain"
 )
 
+type TaskService interface {
+	Add(description string) (int, error)
+	List(filter domain.TaskStatus) ([]domain.Task, error)
+	Delete(taskID int) (bool, error)
+	Update(taskID int, description string) (bool, error)
+	UpdateStatus(taskID int, status domain.TaskStatus) (bool, error)
+}
+
 type CLI struct {
-	tasks *usecase.TaskService
+	tasks TaskService
 	out   io.Writer
 }
 
-func New(tasks *usecase.TaskService, out io.Writer) *CLI {
+func New(tasks TaskService, out io.Writer) *CLI {
 	return &CLI{
 		tasks: tasks,
 		out:   out,
@@ -21,22 +30,113 @@ func New(tasks *usecase.TaskService, out io.Writer) *CLI {
 
 func (c *CLI) Run(args []string) error {
 	if len(args) < 1 {
-		return errors.New("нет аргументов")
+		return errors.New("no command provided")
 	}
 
 	method := args[0]
 	switch method {
 	case "add":
 		if len(args) < 2 {
-			return errors.New("нет описания задачи")
+			return errors.New("task description is required")
 		}
-		err := c.tasks.Add(args[1])
+		id, err := c.tasks.Add(args[1])
 		if err != nil {
 			return err
 		}
-		fmt.Fprintf("Task added successfully (ID: %d)")
+		fmt.Fprintf(c.out, "Task added successfully (ID: %d)", id)
+	case "list":
+		var filter domain.TaskStatus
+		var err error
+		if len(args) > 1 {
+			filter, err = domain.NewTaskStatus(args[1])
+			if err != nil {
+				return err
+			}
+		}
+		tasks, err := c.tasks.List(filter)
+		if err != nil {
+			return err
+		}
+		fmt.Fprintf(c.out, "Tasks: %d", len(tasks))
+		for _, task := range tasks {
+			fmt.Fprintf(c.out, "\n----------------------------------------\n")
+			fmt.Fprintf(c.out, "Task ID: %d\nDescription: %s\nStatus: %s\nCreated: %s\nUpdated: %s", task.Id, task.Description, task.Status, task.CreatedAt, task.UpdatedAt)
+			fmt.Fprintf(c.out, "\n----------------------------------------\n")
+		}
+	case "delete":
+		if len(args) < 2 {
+			return errors.New("task ID is required for delete")
+		}
+		taskID, err := strconv.Atoi(args[1])
+		if err != nil {
+			return err
+		}
+		isDelete, err := c.tasks.Delete(taskID)
+		if err != nil {
+			return err
+		}
+		if isDelete {
+			fmt.Fprintf(c.out, "Task with ID %d was deleted", taskID)
+		} else {
+			fmt.Fprintf(c.out, "Task with ID %d was not found", taskID)
+		}
+	case "update":
+		if len(args) < 3 {
+			return errors.New("task ID and description are required for update")
+		}
+		taskID, err := strconv.Atoi(args[1])
+		if err != nil {
+			return errors.New("task ID must be a number")
+		}
+		newDescription := args[2]
+		if newDescription == "" {
+			return errors.New("task description must not be empty")
+		}
+		isUpdate, err := c.tasks.Update(taskID, newDescription)
+		if err != nil {
+			return err
+		}
+		if isUpdate {
+			fmt.Fprintf(c.out, "Task with ID %d was updated", taskID)
+		} else {
+			fmt.Fprintf(c.out, "Task with ID %d was not found", taskID)
+		}
+	case "mark-in-progress":
+		if len(args) < 2 {
+			return errors.New("task ID is required to update status")
+		}
+		taskID, err := strconv.Atoi(args[1])
+		if err != nil {
+			return errors.New("task ID must be a number")
+		}
+		isUpdate, err := c.tasks.UpdateStatus(taskID, domain.InProgress)
+		if err != nil {
+			return err
+		}
+		if isUpdate {
+			fmt.Fprintf(c.out, "Task with ID %d status was updated", taskID)
+		} else {
+			fmt.Fprintf(c.out, "Task with ID %d was not found", taskID)
+		}
+	case "mark-done":
+		if len(args) < 2 {
+			return errors.New("task ID is required to update status")
+		}
+		taskID, err := strconv.Atoi(args[1])
+		if err != nil {
+			return errors.New("task ID must be a number")
+		}
+		isUpdate, err := c.tasks.UpdateStatus(taskID, domain.Done)
+		if err != nil {
+			return err
+		}
+		if isUpdate {
+			fmt.Fprintf(c.out, "Task with ID %d status was updated", taskID)
+		} else {
+			fmt.Fprintf(c.out, "Task with ID %d was not found", taskID)
+		}
 	default:
-		return errors.New("такого метода не существует")
+		return errors.New("unknown command")
 	}
 	return nil
 }
